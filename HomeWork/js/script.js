31ade0475c8223f3e5313286552b76e52b75713e
Yusif Osmanov <o.yusif@hotmail.com>
2021-04-29 19:37:15 +0400

    Animation on scroll added

---
+++ HomeWork/js/script.js
@@ -0,0 +1,26 @@
+const animationItems = document.querySelectorAll('._anim');
+const animationOnScroll = () => {
+    if (animationItems.length > 0) {
+        for (let i = 0; i < animationItems.length; i++) {
+            const animationItem = animationItems[i];
+            const animationItemHeight = animationItem.offsetHeight;
+            const animationItemOffsetTop = animationItem.offsetParent.offsetTop;
+            const animationStart = 4;
+            let animationItemPoint = window.innerHeight - animationItemHeight / animationStart;
+
+            if (animationItemHeight > window.innerHeight) {
+                animationItemPoint = window.innerHeight - window.innerHeight / animationStart;
+            }
+
+            if ((pageYOffset > (animationItemOffsetTop - animationItemPoint)) && (pageYOffset < (animationItemOffsetTop + animationItemHeight))) {
+                animationItem.classList.add('_active');
+            } else {
+                if (!animationItem.classList.contains('_noRepeat')) {
+                    animationItem.classList.remove('_active');
+                }
+            }
+        }
+    }
+}
+window.addEventListener('scroll', animationOnScroll)
+animationOnScroll();
