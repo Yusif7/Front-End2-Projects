e2938ecf289fd822834e4ed96d937d539121f07e
Yusif Osmanov <o.yusif@hotmail.com>
2021-05-07 17:06:32 +0400

    Clients, news section complete

---
+++ Week02/Sakai/script/script.js
@@ -0,0 +1,9 @@
+// Brands infinite slider
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true
+});
