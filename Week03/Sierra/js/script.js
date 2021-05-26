311fbcf28a10e4c975d4acd342bc6b86e989ece5
Yusif Osmanov <o.yusif@hotmail.com>
2021-05-27 00:15:49 +0400

    Trafalgar header, intro complete

--- Week03/Sierra/js/script.js
+++ Week03/Sierra/js/script.js
@@ -1,4 +1,12 @@
 /* Burger icon */
-document.querySelector('.burger').addEventListener('click', () => {
+/* document.querySelector('.burger').addEventListener('click', () => {
     document.querySelector('.header__nav ul').classList.toggle('active');
+}) */
+
+$(document).ready(() => {
+    $('.burger').on('click', () => {
+
+        $('.header__nav ul').css('transition', 'all 0.6s ease 0s;');
+        $('.header__nav ul').toggleClass('active');
+    })
 })
